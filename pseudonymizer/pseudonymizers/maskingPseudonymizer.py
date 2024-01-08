445a8d3810c42f7e1f86fd72ebceaffc71752017
ksydata <ksydata01@gmail.com>
2024-01-08 20:12:56 +0900

    Trial 4.5 by SY

--- pseudonymizer/pseudonymizers/maskingPseudonymizer.py
+++ pseudonymizer/pseudonymizers/maskingPseudonymizer.py
@@ -7,14 +7,14 @@ from typing import *
 
     
 class MaskingPseudonymizer(Pseudonymizer):
-    def __init__(self, data_type: str, masking_domain: bool):
+    def __init__(self, data_type: str, masking_domain: bool, masking_part: str):
         """data_type은 향후 pseudonymizer.py에서 Pseudonymn 실행 클래스의 
         self._dataframe[column] 개인식별정보의 유형으로 이름, 이메일, 주민등록번호, 사업자등록번호 중 하나로 선언"""
         self.data_type = datatype,
         self.email_masker = EmailMaskingModule(masking_domain)
         self.name_masker = NameMaskingModule()
         self.resident_num_masker = ResidentNumberMaskingModule()
-        self.business_num_masker = BusinessNumberMaskingModule()
+        self.business_num_masker = BusinessNumberMaskingModule(masking_part)
 
     def pseudonymizeData(self, data):
         if data_type == "name":
