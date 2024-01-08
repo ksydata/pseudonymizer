76e3196438486e27bd6d01134203e71d4a31d95c
ksydata <ksydata01@gmail.com>
2024-01-08 19:34:04 +0900

    Trial 4 by SY

---
+++ pseudonymizer/pseudonymizers/maskingPseudonymizer.py
@@ -0,0 +1,30 @@
+from pseudonymizer.pseudonymizers.nameMasking import NameMaskingModule
+from pseudonymizer.pseudonymizers.emailMasking import EmailMaskingModule
+from pseudonymizer.pseudonymizers.residentNumMasking import ResidentNumberMaskingModule
+from pseudonymizer.pseudonymizers.businessNumMasking import BusinessNumberMaskingModule
+import re
+from typing import *
+
+    
+class MaskingPseudonymizer(Pseudonymizer):
+    def __init__(self, data_type: str, masking_domain: bool):
+        """data_type은 향후 pseudonymizer.py에서 Pseudonymn 실행 클래스의 
+        self._dataframe[column] 개인식별정보의 유형으로 이름, 이메일, 주민등록번호, 사업자등록번호 중 하나로 선언"""
+        self.data_type = datatype,
+        self.email_masker = EmailMaskingModule(masking_domain)
+        self.name_masker = NameMaskingModule()
+        self.resident_num_masker = ResidentNumberMaskingModule()
+        self.business_num_masker = BusinessNumberMaskingModule()
+
+    def pseudonymizeData(self, data):
+        if data_type == "name":
+            return self.name_masker.pseudonymzieData(data)
+        elif data_type == "email":
+            return self.email_masker.pseudonymzieData(data)
+        elif data_type == "resident_number":
+            return self.resident_num_masker.pseudonymizeData(data)
+        elif data_type == "business_number":
+            return self.business_num_masker.pseudonymizeData(data)
+        else:
+            raise ValueError("유효한 마스킹 대상 개인식별정보 데이터 타입이 아닙니다.")
+            
