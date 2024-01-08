affc30ce8e1528094ac4b28f95f3e57e4083f03b
ksydata <ksydata01@gmail.com>
2024-01-08 19:33:12 +0900

    Trial 3 by SY

--- pseudonymizer/pseudonymizers/emailMasking.py
+++ pseudonymizer/pseudonymizers/emailMasking.py
@@ -13,18 +13,20 @@ class EmailMaskingModule(Pseudonymizer):
         
     def pseudonymizeData(self, email):
         pattern_match = re.match(
-            r"^[a-zA-z0-9]([-_\.]?[a-zA-Z0-9])*@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}", email)
+            r"^[a-zA-z0-9]([-_\.]?[a-zA-Z0-9])*@[a-zA-Z0-9]+\(.[a-zA-Z]{2,3})", email)
             # .: 정확히 1개 문자 매칭
             # * : 앞 패턴이 0개 이상이어야 함
             # ? : 앞 패턴이 없거나 하나이어야 함
             # + : 1회 이상 반복되는 패턴을 매칭
             # \. : 도메인과 최상위 도메인(TLD)에 대한 구분자 마침표 
             # {,} : 중괄호 안에 표기된 범위만큼 반복되는 패턴을 매칭. {3,5}는 3~5회 매칭을 의미함
+            
         if match:
             local_part = pattern_match.group(0)
             domain_part = pattern_match.group(1)
             tld_part = pattern_math.group(2)
             # local_part, domain_part = email.split("@")
+            
             if self.masking_domain:
                 masked_local_part = re.sub(r"\S", "*", local_part)
                 masked_domain_part = re.sub(r"\S", "*", domain_part)
@@ -39,4 +41,3 @@ class EmailMaskingModule(Pseudonymizer):
     
         # 이메일의 표준은 인터넷 표준 기구(IETF, Internet Engineering Task Force)에서 정의
         # re.sub(pattern, replace, text)
-        # @[a-zA-Z0-9]+\.(.[a-zA-Z]{2,3}))
