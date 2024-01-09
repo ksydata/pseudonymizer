5a8b4e1e772bfb2f6193d3caaadb2d997ac10872
ksydata <ksydata01@gmail.com>
2024-01-09 21:22:11 +0900

    Trial 5 by SY

---
+++ pseudonymizer/privacyPreservingModels.py
@@ -0,0 +1,38 @@
+# ./pseudonymizer/privacyPreservingModels.py
+
+from pseudonymizer.pseudonymizer import Pseudonymizer, Pseudonym
+    # 가명처리 전후 데이터의 속성을 비교하기 위한 유용성 지표 적용 등에 활용하는 방법 고민
+from typing import *
+import pandas as pd
+
+class PrivacyPreservingModel:
+    """
+    동질 집합(Equivalent class)
+    프라이버시 보호 모델에 따른 PPDM(Privacy Preserving Data Mining)
+    ----------------------------------------------------------------
+    개인식별가능정보 속성을 기준으로 데이터를 그룹화하여 프라이버시 보호 모델을 적용하는 클래스
+    """
+    def __init__(self, dataframe):
+        self._dataframe = dataframe
+        # self.pseudo_dataframe = pseudo_dataframe
+        # 가명처리 전이면 pseudo_dataframe = None
+        self.equivalent_class = {}
+        
+    def __str__(self):
+        """주어진 데이터 셋의 컬럼 정보를 반환하는 메서드"""
+        return self._dataframe.info()
+    
+    def categorizeEquivalentClass(self, attribute: List) -> Dict[str, List[str]]:
+        """동질 집합 확인"""
+        groupby_data = self._dataframe.groupby(attribute)
+        for group, data in groupby_data:
+            
+    
+    def K_Anonymity(self):
+        """개별 레코드가 최소한 K개 이상의 동일한 속성값을 가지도록 하는 K-익명성 메서드"""
+        
+    def L_Diversity(self):
+        """각 동질집합 내 특정 민감 속성의 빈도가 L값 이상의 다양성을 가지도록 하는 L-다양성 메서드"""
+    
+    def T_Closeness(self):
+        """민감 정보(SA)의 분포를 전체 데이터 셋의 분포와 유사하도록 하는 T-근접성메서드"""
