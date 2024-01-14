5c70f6206cb85869551ca76bdb3fb2736da9abd2
ksydata <ksydata01@gmail.com>
2024-01-14 17:15:59 +0900

    K-Anonymity Model Trial 1 by SY

--- pseudonymizer/privacyPreservingModels.py
+++ pseudonymizer/privacyPreservingModels.py
@@ -4,6 +4,7 @@ from pseudonymizer.pseudonymizer import Pseudonymizer, Pseudonym
     # 가명처리 전후 데이터의 속성을 비교하기 위한 유용성 지표 적용 등에 활용하는 방법 고민
 from typing import *
 import pandas as pd
+# ./pseudonymizer/pseudonymizer/privacyPreservingModels.py
 
 class PrivacyPreservingModel:
     """개인식별가능정보 속성을 기준으로 데이터를 그룹화하는 클래스"""
@@ -30,13 +31,18 @@ class PrivacyPreservingModel:
                 self.equivalent_class[key] = data.index.tolist()
                 # 동질 집합에 해당하는 행(레코드)의 인덱스 번호를 키 값으로 조회되도록 저장
         
-        return self.equivalent_class
+    def K_Anonymity(self, K: int, sensitive_attributes):
+        """개별 레코드가 최소한 K개 이상의 동일한 속성값을 가지도록 하는 K-익명성 메서드"""
+        for group_key, index_value in self.equivalent_class.items():
+            if len(self._dataframe.loc[index_value, sensitive_attributes]) >= K:
+                print( self._dataframe.loc[index_value, sensitive_attributes], "\n")
+            else:
+                continue
         
-    # def K_Anonymity(self):
-       # """개별 레코드가 최소한 K개 이상의 동일한 속성값을 가지도록 하는 K-익명성 메서드"""
-        
-    # def L_Diversity(self):
-       # """각 동질집합 내 특정 민감 속성의 빈도가 L값 이상의 다양성을 가지도록 하는 L-다양성 메서드"""
+    def L_Diversity(self):
+        """각 동질집합 내 특정 민감 속성의 빈도가 L값 이상의 다양성을 가지도록 하는 L-다양성 메서드"""
+        pass
     
-    # def T_Closeness(self):
-       # """민감 정보(SA)의 분포를 전체 데이터 셋의 분포와 유사하도록 하는 T-근접성메서드"""
+    def T_Closeness(self):
+        """민감 정보(SA)의 분포를 전체 데이터 셋의 분포와 유사하도록 하는 T-근접성메서드"""
+        pass
