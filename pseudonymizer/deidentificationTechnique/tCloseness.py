d92aed9d6f9ccce882ac729f68274fa5eaec86cf
ksydata <ksydata01@gmail.com>
2024-01-29 00:23:27 +0900

    Privacy Preserving Modeling Trial&Error 5 by SY

--- pseudonymizer/deidentificationTechnique/tCloseness.py
+++ pseudonymizer/deidentificationTechnique/tCloseness.py
@@ -28,57 +28,63 @@ class T_Closeness(EquivalentClass):
         # 나누어 떨어지지 않는 여분으로 연산해주어야 할 때 계산
 
         balance_value = len(t_list) - (extra_part)
-        active_loop = True
         # 데이터의 분산도 측정
-            if count_t >= balance_value and active_loop == True:
-            # 여분의 연산으로 하는 부분(나누어 떨어지지 않는 수)에 대하여
-            # 배열의 마지막 부분에서 처리
-            hap = float(hap) / float(total_length)
-            return hap
+        # 여분의 연산으로 하는 부분(나누어 떨어지지 않는 수)에 대하여
+        # 배열의 마지막 부분에서 처리
     """
-
     def __init__(self, dataframe):
         super().__init__(dataframe)
         self.T_data = None
         self.sensitive_attribute = None
         self.tolerance = None
         
-    def checkSensitivesDistribution(self, sensitive_attribute: str):
+    def checkSensitivesDistribution(self, data_set, sensitive_attribute: str):
         """개인식별가능정보(준식별자)의 모든 가능한 조합 n개의 관심 대상값
         (sensitive_attribute)의 분포와 전체 집단의 분포의 거리 최댓값이 <= t로 규정할 때
         분포를 계산하는 메서드"""
-        self.sensitive_attribute = sensitive_attribute
-        self.sensitive_vector = self._dataframe[self.sensitive_attribute]
-        if self.sensitive_vector.dtype =="int" or "float":
-            return prob
-        elif self.sensitive_vector.dtype == "object":
-            distance = {v: count/len(v) 
-                        for (v, count) 
-                        in Counter(self.sensitive_vector).items()}
-        elif self._dataframe[self.sensitive_attribute].dtype == "category":
-            distance = {v: count/len(v) 
-                        for (v, count) 
-                        in Counter(self.sensitive_vector).items()}
+        sensitive_vector = data_set[sensitive_attribute]
+        
+        # 민감속성의 고유한 값과 그 값의 비율 {v: count/len(V)}
+        if sensitive_vector.dtype == "int" or "float":
+            ordered_vector = np.sort(sensitive_vector)
+            distribution = {v: n/len(ordered_vector)  for (v, n)
+                            in ordered_vector, range(len(ordered_vector))}
+        elif sensitive_vector.dtype == "object":
+            distribution = {v: count/len(v) for (v, count) 
+                            in Counter(sensitive_vector).items()}
+        elif sensitive_vector.dtype == "category":
+            distribution = {v: count/len(v) for (v, count) 
+                            in Counter(sensitive_vector).items()}
         else: ValueError("입력받은 {}은 유효한 자료형이 아닙니다."
-                         .format(self._datafrmae[self.sensitive_attribute].dtype))    
+                         .format(self._datafrmae[self.sensitive_attribute].dtype))
+        return distribution
     
-    def earthMoversDistance(self):
+    def earthMoversDistance(self, qi_dist, total_dist):
         """scipy.wasserstein_distance(data_sensitivity, data_population)"""
-        pass
+        # eucdistance = np.sqrt((qi_dist - total_dist)**2)
+        emdistance = np.sum(np.abs(qi_dist - total_dist))
+        return emdistance
 
-    def applyTCloseness(self, attributes: List[str], tolerance: float):
+    def applyTCloseness(self, quasi_identifiers: List[str], tolerance: float, sensitive_attribute: str):
         """tolerance: 허용가능한 확률분포 차이의 범위를 정의하여 T-근접성을 적용하는 메서드"""
-        T_data = dict()
-        self.tolerance = tolerance 
-        # threshold
-        super().categorizeEquivalentClass(attributes)
+        T_data, qi_distribution = dict()
+        self.tolerance = tolerance # threshold
+        vector = self._dataframe[sensitive_attribute]
+        super().categorizeEquivalentClass(quasi_identifiers)
         
         for group_key, index_value in self.equivalent_class.items():
-            distribution_sensitives = self._dataframe.loc[index_value, sensitive_attribute].value_count(normalize = True)
-            # value_counts(normalize = True) = value_counts() / sum 
-            # Earth's Mover Distance
-            self.earthMoversDistance
-            if self.checkTCloseness(distribution_sensitives, self.tolerance):
+            # 1. Empirical Cummulative Probability Distribution
+            qi_distribution[group_key] = self.checkSensitivesDistribution(       
+                vector[index_value], sensitive_attribute)
+            total_distribution[group_key] = self.checkSensitivesDistribution(       
+                vector, sensitive_attribute)
+                # self._dataframe.loc[index_value, sensitive_attribute]
+                # .value_count(normalize = True) = .value_counts() / sum 
+            
+            # 2. Earth's Mover Distance
+            emd = self.earthMoversDistance(qi_distribution, total_distribution)
+            
+            # 3.
+            if emd < tolerance:
                 T_data[group_key] = index_value
-        
         self.T_data = T_data
