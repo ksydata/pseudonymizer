c8b6f26550344d73c1d8e2fdfabc9ae20bf0e752
ksydata <ksydata01@gmail.com>
2024-02-13 20:57:54 +0900

    Add generation method 2 by SY

--- pseudonymizer/pseudonymizers/numcategorization.py
+++ pseudonymizer/pseudonymizers/numcategorization.py
@@ -12,8 +12,10 @@ class CategorizationOfNumeric(Pseudonymizer):
             return self.pseudonymizeAge(input_numeric, grouping_standard)
         elif self.numeric_type == "income":
             return self.pseudonymizeIncome(input_numeric, grouping_standard)
-        elif self.numeric_type == "amount":
-            return self.pseudonymizeAmount(input_numeric, grouping_standard)
+        elif self.numeric_type == "bin":
+            return self.pseudonymizeAmountbyBin(input_numeric, grouping_standard)
+        elif self.numeric_type == "pct":
+            return self.pseudonymizeAmountbyPct(input_numeric, grouping_standard)        
         elif self.numeric_type == "user_definition":
             return self.pseudonymizeDefinition(input_numeric, grouping_standard)
         else: 
@@ -62,17 +64,34 @@ class CategorizationOfNumeric(Pseudonymizer):
                     return f"{index}분위"
         else:
         # While grouping_standard True:
-            grouping_standard 
-            # 오름차순 정렬: 일반적으로 사용하는 버블 정렬은 O(N**2)이므로 병합 정렬 O(NlogN) 활용
-            for index, grouping_standard in enumerate(grouping_)
-            
+            grouping_standard.sort()
+            # 오름차순 정렬 시 일반적으로 사용하는 버블 정렬은 O(N**2)이므로 
+            # 시간복잡도 낮추려면 병합 정렬 O(NlogN) 활용 -> 추후 merge_sort() 메서드 적용하여 리팩토링
+            for index, grouping_standard in enumerate(grouping_standard, start = 1):
+                if income <= grouping_standard:
+                    return f"{index}분위"
     
-    def pseudonymizeAmount(self, amount, grouping_standard):
-        """기타 금액 범주화 메서드
+    def pseudonymizeAmountbyBin(self, amount, grouping_standard):
+        """기타 금액 구간별 범주화 메서드
         신용공여금액(예: 한도/건별대출, 담보대출, 리스/카드할부금융서비스 등)의 일정 급간화
+        """
+        
+    def pseudonymizeAmountbyPct(self, amount, grouping_standard):
+        """기타 금액 백분위에 의한 범주화 메서드
         개인사업자의 추청매출액/평당월임대료를 백분위수에 따라 매출등급화(90~100%, 65~90%, 35~65%, 10~35%, 0~10%)
+        개별 행을 바라보고 가명처리기법을 적용하는 객체에 적합한 메서드인지 의문
         """
         pass
         
-    def pseudonymizeDefinition(self, numeric_tobeclassified. category_mapping: dict):
-        pass
+    def pseudonymizeDefinition(self, numeric_tobeclassified, category_mapping: dict):
+        """사용자가 직접 구간을 설정하도록 하는 범주화 메서드
+        intervals: [(0, 1000), (1000, 5000), (5000, 10000)]
+        """
+        for category, interval in category_mapping.items():
+            if not isinstance(interval, tuple) or len(interval) != 2:
+                return f"유효하지 않은 구간 {interval}입니다."
+            
+            lower, upper = interval # tuple(lower, upper)
+            if lower <= numeric_tobeclassified <= upper:
+                return category
+            return "other types"
