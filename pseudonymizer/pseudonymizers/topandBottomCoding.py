f44f9c163d38c83c2cf7055137c82794558e13b1
ksydata <ksydata01@gmail.com>
2024-03-03 09:32:44 +0900

    top and bottom coding used IQR 2 by SY

--- pseudonymizer/pseudonymizers/topandBottomCoding.py
+++ pseudonymizer/pseudonymizers/topandBottomCoding.py
@@ -2,7 +2,7 @@ from pseudonymizer.pseudonymizer import Pseudonymizer
 
 class TopandBottomCoding(Pseudonymizer):
     """적은 수의 분포를 가진 양 끝단의 정보를 범주화 등의 기법을 적용하여 식별성을 낮추는 가명처리기법 구체클래스"""
-    def __init__(self, outlier_type):
+    def __init__(self, outlier_type, bounded_value):
         self.outlier_type = outlier_type
         self.bounded_value = bounded_value
     
@@ -20,15 +20,29 @@ class TopandBottomCoding(Pseudonymizer):
         다만, 다변량 설정이나 고차원 데이터에는 유용성이 떨어진다는 한계
         """
         Q1, _, Q3 = self.calculateQuartiles(dataseries)
-        lower_bound = 
-        upper_bound = 
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5*IQR
+        upper_bound = Q3 + 1.5*IQR
+        
+        datatopcoding = dataseries.apply(
+          lambda x: x if x <= upper_bound
+          else upper_bound)
+        databottomcoding = datatopcoding.apply(
+          lambda x: x if x >= lower_bound
+          else lower_bound)
+        return databottomcoding
 
     def pseudonymizeOutlierPct(self, dataseries):
         """
+        백분율을 기준으로 상하단 경계값을 설정
         """
-        lower_bound = 
-        upper_bound = 
-    
+        if self.bounded_value < 0.5:
+            lower_bound = self.bounded_value
+            upper_bound = 1 - lower_bound
+        else:
+            upper_bound = self.bounded_value
+            lower_bound = 1 - upper_bound
+
     def calculateMedian(self, data):
         """중간값(백분위50%) 계산하는 메서드"""
         n = len(data)
@@ -39,12 +53,16 @@ class TopandBottomCoding(Pseudonymizer):
         else:
             return (data[n//2-1] + data[n//2])/2
     
+    def sort(self, data):
+        pass
+
     def calculateQuartiles(self, dataseries):
         """백분위25%(Q1)과 백분위75%(Q3)를 중간값을 이용하여 구하는 메서드"""
         data = dataseries.to_list()
         # 빠른 quick정렬 알고리즘(heap, merge으로도 대체 가능)
         data
         
+        # 백분위 25%(가장 작은 수부터 중간값까지 수의 중간값), 백분위 75%(중간값부터 가장 큰 수까지 수의 중간값)
         Quartile2 = self.calculateMedian(data)
         Quartile1 = self.calculateMedian(data[:Q2])
         Quartile3 = self.calculateMedian(data[Q2:])
