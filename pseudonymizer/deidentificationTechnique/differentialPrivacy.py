17b2258fe0a3da5a75b34327ac21e878641835db
ksydata <ksydata01@gmail.com>
2024-03-07 20:50:37 +0900

    Modify apply laplace mechanism by SY

--- pseudonymizer/deidentificationTechnique/differentialPrivacy.py
+++ pseudonymizer/deidentificationTechnique/differentialPrivacy.py
@@ -2,22 +2,13 @@
 
 from pseudonymizer.pseudonymizer.deidentificationTechnique.equivalentClass import EquivalentClass
 from typing import *
-
-# ./pseudonymizer/pseudonymizer/deidentificationTechnique/differentialPrivacy.py
-
-# from pseudonymizer.pseudonymizer.deidentificationTechnique.equivalentClass import EquivalentClass
-# from typing import *
-
-# ./pseudonymizer/pseudonymizer/deidentificationTechnique/differentialPrivacy.py
-
-# from pseudonymizer.pseudonymizer.deidentificationTechnique.equivalentClass import EquivalentClass
-# from typing import *
+from scipy.stats import laplace
 
 class DifferentialPrivacy(EquivalentClass):
     """차분 프라이버시 클래스
     차등적 정보보호 기능을 수행"""
     
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, self.ratio_bounded: float):
         super().__init__(dataframe)
         # 동질집합을 키로하고 인덱스 번호를 값으로 하는 신뢰구간 초과하는 값을 
         self.upperoutlier_dictionary = {}
@@ -25,18 +16,21 @@ class DifferentialPrivacy(EquivalentClass):
         self.sensitive_attribute = None
         
         # ratio_bounded = epsilon(개인정보(가명정보) 보호 수준 결정)
-        self.ratio_bounded = None
-        self.sensitivity = None
+        self.ratio_bounded = self.ratio_bounded
     
     def dataDeviatingfromCI(self, boundary: float, attributes: List[str], sensitive_attribute: str):
-        """동질집합 내 평균에서 양쪽 3표준편차의 범위 99.7%에 들지 않는 민감정보 행 번호만 별도로 추출하는 메서드"""
+        """동질집합 내 평균에서 양쪽 3표준편차의 범위 99.7%에 들지 않는 민감정보 행 번호만 별도로 추출하는 메서드
+        가설검정 기반에서 노이즈를 입력하는 방식인 GDP(Gaussian Differential Privacy) 방법
+        이 방식은 통계 가설 관점에서 주어진 유의수준에서 최소의 제2종 오류를 trade-off function으로 나타냄으로써 차분 정보보호를 적용
+        """
         super().categorizeEquivalentClass(attributes)
+        self.sensitive_attribute = sensitive_attribute
         for group_key, index_value in self.equivalent_class.items():
             mu = np.nanmean(self._dataframe.loc[index_value, sensitive_attribute])
             sigma = np.nanstd(self._dataframe.loc[index_value, sensitive_attribute])
             
             for i in index_value:
-                x = self._dataframe.loc[i, sensitive_attribute]
+                x = self._dataframe.loc[i, self.sensitive_attribute]
                 if mu-boundary*sigma <= x <= mu+boundary*sigma:
                     pass
                 elif x > mu+boundary*sigma:
@@ -51,26 +45,51 @@ class DifferentialPrivacy(EquivalentClass):
         특정 결과를 얻기 위한 쿼리 K를 각 데이터에 적용한 결과인 K(D1)와 K(D2)가 동일한 분포 S에 속할
         확률의 비율(두 데이터 분포의 차이)을 일정 수준(epsilon)보다 작도록 함
         """
-        pass
-    
-    def gaussianMechanism(self, ratio_bounded: float):
-        """가우스 메커니즘을 적용하여 특정 데이터 행에 랜덤 노이즈값을 추가하는 메서드"""
-        sigma = self.sensitivity / ratio_bounded
-        noise = np.random.normal(0, sigma, len(data))
-        return data + noise
+        for group_key, outlier_list self.upperoutlier_dictionary.items():
+            for i in outlier_list:
+                group_data, exception_data = 0, 0
+                group_list = self.equivalent_class[group_key]
+                exception_list = group_list.remove(i)
+                
+                group_data = self._dataframe.loc[group_list, self.sensitive_attribute]
+                exception_list = self._dataframe.loc[exception_list, self.sensitive_attribute]
+                
+                
+    @staticmethod
+    def estimateLaplaceParameters(self, data):
+        """라플라스 분포의 모수 평균(mu)과 스케일(beta)을 추정하는 메서드"""
+        mu = np.nanmean(data)
+        beta = np.mean(np.abs(data - mu))
+        return mu, beta
 
-    def laplaceMechanism(self, ratio_bounded: float):
+    @staticmethod
+    def laplacePDF(self, x, mu, beta):
+        """라플라스 연속확률분포 확률밀도함수
+        확률분포(확률변수가 특정한 값을 가질 확률을 나타내는 함수)"""
+        return (1 / (2*beta)) * exp(-abs(x-mu) / beta)
+    
+    def calculateProbabilityRatio(self, include_data, exclude_data):
+        """확률변수 x가 라플라스 분포에 속할 확률과 특정 행의 포함 여부 데이터 간 비율을 계산하는 메서드"""
+        # 두 데이터의 평균과 스케일 파라미터를 추정
+        mu_include, beta_include = self.estimateLaplaceParameters(include_data)
+        mu_exclude, beta_exclude = self.estimateLaplaceParameters(exclude_data)
+        
+        # 두 데이터의 라플라스 분포에 속할 확률 계산
+        prob_include = self.laplacePDF(include_data, mu_include, beta_include)
+        prob_exclude = self.laplacePDF(exclude_data, mu_exclude, beta_exclude)
+        prob_ratio = prob_include / prob_exclude
+        
+        # 두 데이터의 라플라스 분포에 속할 확률의 비율 계산
+        return prob_ratio
+
+    def laplaceMechanism(self, include_data, exclude_data, particular_record):
         """라플라스 메커니즘을 적용하여 특정 데이터 행에 랜덤 노이즈값을 추가하는 메서드"""
-        beta = self.sensitivity / ratio_bounded
-        noise = np.random.laplace(0, beta, len(data))
-        return data + noise
-    
-    @staticmethod
-    def gaussianPDF(self, x, mu, sigma):
-        """정규분포(가우시안) 확률밀도함수"""
-        return (1 / sqrt(2*pi*sigma**2)) * exp(-0.5*((x-mu) / sigma)**2)
-
-    @staticmethod
-    def laplacePDF(self, x, mu, scale_param):
-        """라플라스분포 확률밀도함수"""
-        return (1 / (2*scale_param)) * exp(-abs(x-mu) / scale_param)
+        # 전역 민감도 계산
+        sensitivity = self.calculateProbabilityRatio(include_data, exclude_data)
+        # 사용자 지정 개인정보 보호 수전 하이퍼파라미터 엡실론을 통해 스케일 파라미터 정의
+        beta = sensitivity / self.ratio_bounded
+        # 평균 0, 베타를 분산으로 가지는 라플라스 분포에 속하는 랜덤 난수 추출
+        noise = np.random.laplace(0, beta, len(include_data))
+        
+        # 이상치에 노이즈 추가
+        return particular_record + noise
